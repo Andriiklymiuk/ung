4d8d1464c0840a27f8a13f7d07d429e565dcf7ee
Andrii Klymiuk <ext-andrii.klymiuk@skeepers.io>
2025-11-28 12:07:18 +0100

    Ung macos

---
+++ ung-macos/ung/ungApp.swift
@@ -0,0 +1,17 @@
+//
+//  ungApp.swift
+//  ung
+//
+//  Created by Andrii Klymiuk on 28.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct ungApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
